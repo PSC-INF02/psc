283b3930633b7fa9fd345cefa66efaa14193d49e
Schrottenloher <andre.schrottenloher@polytechnique.edu>
2015-03-05 22:48:51 +0100

    Added samples (not cleaned) in demo

--- src/demo.py
+++ src/demo.py
@@ -23,7 +23,37 @@ from abstracter.crawler.parse_crawler import * #download_and_parse_data,download
 
 from abstracter.workspace.entities_recognition import *
 from abstracter import *
-context=Context()
 
+import networkx as nx
 #activate_entities(context,["wayne_rooney"])
-print(match_entities(context,["wayne rooney","wayne"]))
+context=Context()
+#print(match_entities(context,["wayne rooney","wayne"]))
+
+liste=["Everton","Goodison Park","Roberto Martinez","Martinez","Champions League","Diego Simeone","Atletico Madrid",
+"FIFA","David Moyes","Barcelona","Real Sociedad","New Year's Day","Premier League","Roberto"];
+print(match_entities(context,liste))
+
+#print(minimize_distance("soccer_player",["the soccer player"]))
+
+
+
+#print(context.network.shortest_path("wayne_rooney","bedroom"))
+
+
+#for g in nx.weakly_connected_component_subgraphs(context.network.network):
+#	print(nx.average_shortest_path_length(g))
+
+text="""
+Everton are on their worst run since December 2005, but the Goodison Park fans must get behind Roberto Martinez
+Martinez was praised last year for his style of play that took Everton to the brink of Champions League qualification
+Talk of Martinez needing to change his style is baffling Diego Simeone's work at Atletico Madrid has been 
+unbelievable, I hope he wins FIFA's coach of the year award 
+He was darling of Goodison Park this time last year, hailed by supporters for bringing back ‘the School of Science’.
+Having got Everton playing a slick, fast brand of football that almost took them into the Champions League, Roberto Martinez could 
+do no wrong but, suddenly, he has become the topic of an increasingly heated debate.The argument? He needs to change and get7
+ Everton playing a more solid and direct style, like David Moyes favoured. Maybe some watched Real Sociedad’s backs-to-the-wall 
+ victory against Barcelona and were reminded of similar big wins that their former manager had inspired.       
+    Roberto Martinez is under pressure at Everton with his team lying just four points ahead of the relegation zone      
+    Everton have been struggling in the Premier League and have failed to hit the heights of last season  
+A 2-0 defeat by Hull on New Year's Day was Everton's fourth Premier League loss in a row
+"""
