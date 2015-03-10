de77a03da70c65e1cd42bd579c4d5d0f71f98ba6
Théophane Hufschmitt <theophane.hufschmitt@polytechnique.edu>
2015-03-10 11:35:49 +0100

    Made adapter.py respect the conventions
    Because it was horrible ;)

--- src/abstracter/adapter/adapter.py
+++ src/abstracter/adapter/adapter.py
@@ -1,58 +1,67 @@
 
 
 class adapter:
-	"""
-	A class that takes an article analyzed by systran and pours elements from it into a workspace
-	Use it with
-	adapter.parse(filename, workspace);
-	"""
+    """
+    A class that takes an article analyzed by systran
+    and pours elements from it into a workspace
+    Use it with
+    adapter.parse(filename, workspace);
+    """
 
-	def parse(self, filename, workspace):
-		"""
-		Parses a syntactic tree (systran format) and fills workspace given as argument with new information.
-		"""
-		f = open(filename, 'r')
-		for line in f:
-			# Each line is a sentence
-			words = line.split(' ')
-			for word in words:
-				# Spaces serve as a delimiter for words in a sentence
-				self.parseWord(word, workspace)
+    def parse(self, filename, workspace):
+        """
+        Parses a syntactic tree (systran format) and fills workspace
+        given as argument with new information.
+        """
+        f = open(filename, 'r')
+        for line in f:
+            # Each line is a sentence
+            words = line.split(' ')
+            for word in words:
+                # Spaces serve as a delimiter for words in a sentence
+                self.parseWord(word, workspace)
 
-	def parseWord(self, word, workspace):
-		"""
-		Takes a cursory glance at whatever information is stored in the word (systran format) and adds it to the workspace
-		"""
-		wordStatus = word.split('-|-')
+    def parseWord(self, word, workspace):
+        """
+        Takes a cursory glance at whatever information is stored in the word
+        (systran format) and adds it to the workspace
+        """
+        wordStatus = word.split('-|-')
 
-		# Getting lemma and grammatical class
-		lemma = wordStatus.pop(1)
-		nature = wordStatus.pop(1)
+        # Getting lemma and grammatical class
+        lemma = wordStatus.pop(1)
+        nature = wordStatus.pop(1)
 
-		'''
-		 Now there are two possibilities : either the name is just a name and I can push it in the workspace as is, or it's an adjective / verb or such and I need to make it an attribute or an event and to gather some more information.
-		'''
-		
-		if 'noun' in nature:
-			#I'll read doc to see what I can get into a node
-			pass
+        '''
+         Now there are two possibilities :
+             - the name is just a name and I can push it in the workspace as is
+             - it's an adjective / verb or such and I need to make it
+             an attribute or an event and to gather some more information.
+        '''
 
-		else:
+        if 'noun' in nature:
+            # I'll read doc to see what I can get into a node
+            pass
 
-			# Getting features and relations to other words in the sentence (just in case)
+        else:
 
-			features=wordStatus.pop(1).split(';')
-		
-			'''
-			For now I'll stay simple and just ignore everything until the relations in the sentence ; there might be some useful info in those features though.
-			'''
-			while not 'oldtag' in features.pop():
-				pass
+            # Getting features and relations to other words in the sentence
+            # (just in case)
 
-			if 'adj' in nature:
-				#Seek some particular tag that should be useful, add node
-				pass
+            features = wordStatus.pop(1).split(';')
 
-			if 'verb' in nature:
-				#Seek some particular tag that should be useful, add node
-				pass
+            '''
+            For now I'll stay simple and just ignore everything
+            until the relations in the sentence ;
+            there might be some useful info in those features though.
+            '''
+            while 'oldtag' not in features.pop():
+                pass
+
+            if 'adj' in nature:
+                # Seek some particular tag that should be useful, add node
+                pass
+
+            if 'verb' in nature:
+                # Seek some particular tag that should be useful, add node
+                pass
