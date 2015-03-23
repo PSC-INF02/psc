f250ac3cfd73d5c117463f46aef250fb7b815b21
Schrottenloher <andre.schrottenloher@polytechnique.edu>
2015-03-23 10:08:53 +0100

    Completed the algorithm.

--- src/abstracter/util/anaphora_resolution.py
+++ src/abstracter/util/anaphora_resolution.py
@@ -5,9 +5,22 @@ Resolve anaphors.
 
 NOUN_PHRASES_TYPES = ["noun:propernoun", "noun:common", "noun:acronym", "det", "adj", "numeric"]
 PRONOUN_TAGS = ["pron"]
+
+###################################
+# heuristically, only words tagged as objects or agents of verbs
+# make good noun phrases
+# however, this is not always the case -> have to run some tests
+##############################
+
 MASTER_NOUN_TAGS = ["object_of_verb", "agent_of_verb"]
 TO_ADD = ["modified_on_left"]
 TO_BE_ADDED = ["modifies_right_head", "modifies_another_noun", "semantic_modifier_of"]
+DEFINITE = ["the", "his", "her", "this", "these"]
+INDEFINITE = ["a"]
+
+
+def _get_norm(sent, id):
+    return sent[id]["norm"]
 
 
 def _get_tag(sent, id, tag):
@@ -16,6 +29,18 @@ def _get_tag(sent, id, tag):
 
 def _has_type_in(sent, id, type_list):
     return sent[id]["type"] in type_list
+
+
+def is_definite(sent, id_list):
+    """
+    Check if it is a definite noun phrase, i.e
+    if it contains a definite article or
+    a possessive or demonstrative pronoun.
+    """
+    res = False
+    for id in id_list:
+        res = res or sent[id]["norm"] in DEFINITE
+    return res
 
 
 def _has_tag_in(sent, id, tag_list):
@@ -86,32 +111,65 @@ def resolve_anaphoras(sent, np, previous_np1=None, previous_np2=None):
     @warning : take in account the way noun phrases in the previous sentences
     are numbered.
 
+    @param np Noun phrases (dict).
     @param sent Dict.
     @return A dict of relations between the pronouns and noun phrases,
     identified with their master noun.
     """
     res = {}
+    np_keys = sorted(np)
+    # print(np_keys)
     # recognize pronouns
     for term_id in sent:
         if _has_type_in(sent, term_id, PRONOUN_TAGS):
             res[term_id] = {}
-    # check antecedent data
-    # check gender, etc
     for term_id in res:
         for term_id2 in np:
             # proximity between noun phrase and pronoun
+            # + the noun phrase has to appear before the pronoun
             res[term_id][term_id2] = term_id2 - term_id if term_id > term_id2 else -100
 
             # add here : taking into account previous phrases
+
+            # check gender, etc
             for tag in DISCRIMINATIVE_TAGS:
                 if (_get_tag(sent, term_id, tag) and _get_tag(sent, term_id2, tag)
                    and not _get_tag(sent, term_id, tag) == _get_tag(sent, term_id2, tag)):
                     res[term_id][term_id2] -= 100
+        # check antecedent data (given by systran)
         if _has_tag_in(sent, term_id, ANTECEDENT_TAGS):
             for tag in ANTECEDENT_TAGS:
                 if _get_tag(sent, term_id, tag):
                     res[term_id][_get_tag(sent, term_id, tag)] = 1
     # other checkings
+    # 1 : noun phrases that appear at the beginning of a sentence
+    # get a better score
+    first = np_keys[0]
+    for term_id in res:
+        res[term_id][first] += 1
+    # 2 : indicating verbs ?
+
+    # 3 : lexical reiteration (with previous sentences also)
+    # we check the head nouns
+    # each repeted head noun gains 1
+    # we can include here synonymy data
+    for id in np_keys:
+        temp = 0
+        for id2 in np_keys:
+            if _get_norm(sent, id) == _get_norm(sent, id2):
+                temp += 1
+        if temp > 1:
+            for term_id in res:
+                res[term_id][id] += 1
+
+    # 4 : prepositional or non prepositional NP : "into the VCR"
+    # ranking : subject, direct object, indirect object
+
+    # 5 : collocation pattenrn
+
+    # 6 :immediate reference
+
+    # 7 : term preference
 
     # get the better
     result = {}
@@ -124,6 +182,11 @@ def resolve_anaphoras(sent, np, previous_np1=None, previous_np2=None):
                 best = term_id2
         result[term_id] = best
     print(result)
+
+
+def print_resolution(sent, np, resolve_result):
+    for i in sorted(sent):
+        print(sent[i])
 
 
 def resolve_periphrasis(sent, noun_phrases):
