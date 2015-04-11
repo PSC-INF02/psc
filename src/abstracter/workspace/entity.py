2b270b13563cd3786a7d0d2513e9dd47e21a909e
XBelette <antonin.angibault@polytechnique.edu>
2015-04-11 17:48:06 +0200

    Added entity class, meant to represent a real world entity that we met in the text.

---
+++ src/abstracter/workspace/entity.py
@@ -0,0 +1,45 @@
+
+
+class entity:
+	'''
+	Represents an entity in the text, as in an actual being in the real world that we've seen in the text.
+	it contains :
+	* a name
+	* a list of the attributes that reference to it
+	* a list of references where the entity is referred to in the text, under the norm (paragraphNumber, wordNumber)
+	'''
+
+	def __init__(self, name=None, attributes=None, references=None):
+		'''
+		Initianalises an entity. All arguments but the name are optional.
+		@param name the name of the entity
+		@param attributes a list of the attribute ids applying to the entity
+		@param references a list of places where that entity is referred to in the text
+		@returns an entity ready to be pushed in the workspace !
+		'''
+
+		self.name = name
+		if attributes:
+			self.attributes = attributes
+		else:
+			self.attributes = []
+		if references:
+			self.references = references;
+		else:
+			self.references = []
+
+	def add_attribute(self, attributeID):
+		'''
+		Adds the id of an attribute object to the attributes list
+		@param attributeID the ID of the attribute to add
+		'''
+
+		self.attributes.append(attributeID)
+
+	def add_reference(self, reference):
+		'''
+		Adds another reference to the references list
+		@param reference a reference tuple : (paragraphNumber, wordNumber)
+		'''
+
+		self.references.append(reference)
