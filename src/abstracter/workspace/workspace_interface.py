654c3b067435a55bcd9f6aeaeda0e00edaed14c2
Théophane Hufschmitt <theophane.hufschmitt@polytechnique.edu>
2015-04-15 07:59:59 +0200

    Modified and commented workspace interface

--- src/abstracter/workspace/workspace_interface.py
+++ src/abstracter/workspace/workspace_interface.py
@@ -3,11 +3,22 @@ class AbstractNetwork:
     An abstract class containing the interface a network must have
     """
 
-    def add_sentence(self):
-        raise NotImplementedError("Should have implemented this")
+    def add_word(self, parid, word):
+        """
+        Adds a word to the workspace
 
-    def get_sentence(self, sentID):
+        @param parid the id of the paragraph the word belongs to
+        @param word the word
+        """
         raise NotImplementedError("Should have implemented this")
 
     def get_word(self, wid):
+        """
+        Get a word from the workspace and return null if it is absent
+        """
         raise NotImplementedError("Should have implemented this")
+
+    def get_or_add_word(self, wid):
+        """
+        Get a word from the workspace, creating it if absent
+        """
