8d12c96bedfb14e9ffcf758bb3421bb8e8103e21
Nadrieril <nadrieril@gmail.com>
2015-04-20 17:22:13 +0200

    Improve systran parser tag handling

--- src/abstracter/util/systran_parser.py
+++ src/abstracter/util/systran_parser.py
@@ -9,69 +9,116 @@ import json
 
 
 TAGS_INFO = {
-        "abstract": {"keep": True, "cor": "ABSTRACT", "is_rel": False},
-        "agent_of_action": {"keep": True, "cor": "AGENT_OF_ACTION", "is_rel": True},
-        "agent_of_verb": {"keep": True, "cor": "AGENT_OF_VERB", "is_rel": True},
-        "antecedent": {"keep": True, "cor": "ANTECEDENT", "is_rel": True},
-        "antecedent_of_relpro": {"keep": True, "cor": "ANTECEDENT_OF_RELPRO", "is_rel": True},
-        "apposition_to": {"keep": True, "cor": "APPOSITION_TO", "is_rel": True},
-        "closing_quote_ptr": {"keep": False, "cor": "CLOSING_QUOTE_PTR", "is_rel": True},
-        "comparative": {"keep": True, "cor": "COMPARATIVE", "is_rel": False},
-        "det": {"keep": True, "cor": "DET", "is_rel": False},
-        "dirobj": {"keep": True, "cor": "DIROBJ", "is_rel": True},
-        "dirobj_of": {"keep": True, "cor": "DIROBJ_OF", "is_rel": True},
-        "emphatic": {"keep": False, "cor": "EMPHATIC", "is_rel": False},
-        "event": {"keep": True, "cor": "EVENT", "is_rel": False},
-        "future": {"keep": True, "cor": "FUTURE", "is_rel": False},
-        "govern": {"keep": True, "cor": "GOVERN", "is_rel": True},
-        "governed_by": {"keep": True, "cor": "GOVERNED_BY", "is_rel": True},
-        "governs_another_noun": {"keep": True, "cor": "GOVERNS_ANOTHER_NOUN", "is_rel": True},
-        "governs_nonfinite": {"keep": False, "cor": "GOVERNS_NONFINITE", "is_rel": True},
-        "has_apposition": {"keep": True, "cor": "HAS_APPOSITION", "is_rel": True},
-        "human": {"keep": True, "cor": "HUMAN", "is_rel": False},
-        "indirobj": {"keep": True, "cor": "INDIROBJ", "is_rel": True},
-        "indirobj_of": {"keep": True, "cor": "INDIROBJ_OF", "is_rel": True},
-        "infinitive": {"keep": True, "cor": "INFINITIVE", "is_rel": False},
-        "linked_to_predicate": {"keep": False, "cor": "LINKED_TO_PREDICATE", "is_rel": True},
-        "location": {"keep": False, "cor": "LOCATION", "is_rel": False},
-        "modified_by_adj": {"keep": True, "cor": "MODIFIED_BY_ADJ", "is_rel": True},
-        "modified_by_adverb": {"keep": True, "cor": "MODIFIED_BY_ADVERB", "is_rel": True},
-        "modified_by_prep1": {"keep": True, "cor": "MODIFIED_BY_PREP1", "is_rel": True},
-        "modified_by_prep2": {"keep": True, "cor": "MODIFIED_BY_PREP2", "is_rel": True},
-        "modified_on_left": {"keep": True, "cor": "MODIFIED_ON_LEFT", "is_rel": True},
-        "modified_on_right": {"keep": True, "cor": "MODIFIED_ON_RIGHT", "is_rel": True},
-        "modifies": {"keep": True, "cor": "MODIFIES", "is_rel": True},
-        "modifies_another_noun": {"keep": True, "cor": "MODIFIES_ANOTHER_NOUN", "is_rel": True},
-        "modifies_left_head": {"keep": True, "cor": "MODIFIES_LEFT_HEAD", "is_rel": True},
-        "modifies_right_head": {"keep": True, "cor": "MODIFIES_RIGHT_HEAD", "is_rel": True},
-        "nb": {"keep": False, "cor": "NB", "is_rel": False},
-        "neg": {"keep": False, "cor": "NEG", "is_rel": False},
-        "next_enum": {"keep": False, "cor": "NEXT_ENUM", "is_rel": True},
-        "nfw": {"keep": True, "cor": "NFW", "is_rel": False},
-        "nonfinite_governed_by": {"keep": False, "cor": "NONFINITE_GOVERNED_BY", "is_rel": True},
-        "object_of_action": {"keep": True, "cor": "OBJECT_OF_ACTION", "is_rel": True},
-        "object_of_verb": {"keep": True, "cor": "OBJECT_OF_VERB", "is_rel": True},
-        "oldtag": {"keep": False, "cor": "OLDTAG", "is_rel": True},
-        "opening_quote_ptr": {"keep": False, "cor": "OPENING_QUOTE_PTR", "is_rel": True},
-        "passive": {"keep": True, "cor": "PASSIVE", "is_rel": False},
-        "past": {"keep": True, "cor": "PAST", "is_rel": False},
-        "per": {"keep": False, "cor": "PER", "is_rel": False},
-        "perfect": {"keep": True, "cor": "PERFECT", "is_rel": False},
-        "physub": {"keep": False, "cor": "PHYSUB", "is_rel": False},
-        "pl": {"keep": True, "cor": "PL", "is_rel": False},
-        "predadj_of": {"keep": True, "cor": "PREDADJ_OF", "is_rel": True},
-        "present": {"keep": True, "cor": "PRESENT", "is_rel": False},
-        "previous_enum": {"keep": False, "cor": "PREVIOUS_ENUM", "is_rel": True},
-        "progressive": {"keep": True, "cor": "PROGRESSIVE", "is_rel": False},
-        "refl": {"keep": False, "cor": "REFL", "is_rel": False},
-        "semantic_modifier_of": {"keep": True, "cor": "SEMANTIC_MODIFIER_OF", "is_rel": True},
-        "sg": {"keep": True, "cor": "SG", "is_rel": False},
-        "skip_begin": {"keep": False, "cor": "SKIP_BEGIN", "is_rel": True},
-        "skip_end": {"keep": False, "cor": "SKIP_END", "is_rel": True},
-        "superlative": {"keep": True, "cor": "SUPERLATIVE", "is_rel": False},
-        "time": {"keep": True, "cor": "TIME", "is_rel": False},
+    "abstract": {"id": "ABSTRACT", "type": "boolean"},
+    "agent_of_action": {"id": "AGENT_OF_ACTION", "type": "relation"},
+    "agent_of_verb": {"id": "AGENT_OF_VERB", "type": "relation"},
+    "antecedent": {"id": "ANTECEDENT", "type": "relation"},
+    "antecedent_of_relpro": {"id": "ANTECEDENT_OF_RELPRO", "type": "relation"},
+    "apposition_to": {"id": "APPOSITION_TO", "type": "relation"},
+    "closing_quote_ptr": {"id": "CLOSING_QUOTE_PTR", "type": "relation"},
+    "comparative": {"id": "COMPARATIVE", "type": "boolean"},
+    "det": {"id": "DET", "type": "unknown"},
+    "dirobj": {"id": "DIROBJ", "type": "relation"},
+    "dirobj_of": {"id": "DIROBJ_OF", "type": "relation"},
+    "emphatic": {"id": "EMPHATIC", "type": "boolean"},
+    "event": {"id": "EVENT", "type": "boolean"},
+    "future": {"id": "FUTURE", "type": "boolean"},
+    "govern": {"id": "GOVERN", "type": "relation"},
+    "governed_by": {"id": "GOVERNED_BY", "type": "relation"},
+    "governs_another_noun": {"id": "GOVERNS_ANOTHER_NOUN", "type": "relation"},
+    "governs_nonfinite": {"id": "GOVERNS_NONFINITE", "type": "relation"},
+    "has_apposition": {"id": "HAS_APPOSITION", "type": "relation"},
+    "human": {"id": "HUMAN", "type": "boolean"},
+    "indirobj": {"id": "INDIROBJ", "type": "relation"},
+    "indirobj_of": {"id": "INDIROBJ_OF", "type": "relation"},
+    "infinitive": {"id": "INFINITIVE", "type": "boolean"},
+    "linked_to_predicate": {"id": "LINKED_TO_PREDICATE", "type": "relation"},
+    "location": {"id": "LOCATION", "type": "boolean"},
+    "modified_by_adj": {"id": "MODIFIED_BY_ADJ", "type": "relation"},
+    "modified_by_adverb": {"id": "MODIFIED_BY_ADVERB", "type": "relation"},
+    "modified_by_prep1": {"id": "MODIFIED_BY_PREP1", "type": "relation"},
+    "modified_by_prep2": {"id": "MODIFIED_BY_PREP2", "type": "relation"},
+    "modified_on_left": {"id": "MODIFIED_ON_LEFT", "type": "relation"},
+    "modified_on_right": {"id": "MODIFIED_ON_RIGHT", "type": "relation"},
+    "modifies": {"id": "MODIFIES", "type": "relation"},
+    "modifies_another_noun": {"id": "MODIFIES_ANOTHER_NOUN", "type": "relation"},
+    "modifies_left_head": {"id": "MODIFIES_LEFT_HEAD", "type": "relation"},
+    "modifies_right_head": {"id": "MODIFIES_RIGHT_HEAD", "type": "relation"},
+    "nb": {"id": "NB", "type": "unknown"},
+    "neg": {"id": "NEG", "type": "boolean"},
+    "next_enum": {"id": "NEXT_ENUM", "type": "relation"},
+    "nfw": {"id": "NFW", "type": "boolean"},
+    "nonfinite_governed_by": {"id": "NONFINITE_GOVERNED_BY", "type": "relation"},
+    "object_of_action": {"id": "OBJECT_OF_ACTION", "type": "relation"},
+    "object_of_verb": {"id": "OBJECT_OF_VERB", "type": "relation"},
+    "oldtag": {"id": "OLDTAG", "type": "relation"},
+    "opening_quote_ptr": {"id": "OPENING_QUOTE_PTR", "type": "relation"},
+    "passive": {"id": "PASSIVE", "type": "boolean"},
+    "past": {"id": "PAST", "type": "boolean"},
+    "per": {"id": "PER", "type": "unknown"},
+    "perfect": {"id": "PERFECT", "type": "boolean"},
+    "physub": {"id": "PHYSUB", "type": "boolean"},
+    "pl": {"id": "PL", "type": "boolean"},
+    "predadj_of": {"id": "PREDADJ_OF", "type": "relation"},
+    "present": {"id": "PRESENT", "type": "boolean"},
+    "previous_enum": {"id": "PREVIOUS_ENUM", "type": "relation"},
+    "progressive": {"id": "PROGRESSIVE", "type": "boolean"},
+    "refl": {"id": "REFL", "type": "boolean"},
+    "semantic_modifier_of": {"id": "SEMANTIC_MODIFIER_OF", "type": "relation"},
+    "sg": {"id": "SG", "type": "boolean"},
+    "skip_begin": {"id": "SKIP_BEGIN", "type": "relation"},
+    "skip_end": {"id": "SKIP_END", "type": "relation"},
+    "superlative": {"id": "SUPERLATIVE", "type": "boolean"},
+    "time": {"id": "TIME", "type": "boolean"}
 }
 
+KEPT_TAGS = set([
+    "abstract",
+    "agent_of_action",
+    "agent_of_verb",
+    "antecedent",
+    "antecedent_of_relpro",
+    "apposition_to",
+    "comparative",
+    "det",
+    "dirobj",
+    "dirobj_of",
+    "event",
+    "future",
+    "govern",
+    "governed_by",
+    "governs_another_noun",
+    "has_apposition",
+    "human",
+    "indirobj",
+    "indirobj_of",
+    "infinitive",
+    "modified_by_adj",
+    "modified_by_adverb",
+    "modified_by_prep1",
+    "modified_by_prep2",
+    "modified_on_left",
+    "modified_on_right",
+    "modifies",
+    "modifies_another_noun",
+    "modifies_left_head",
+    "modifies_right_head",
+    "neg",
+    "nfw",
+    "object_of_action",
+    "object_of_verb",
+    "passive",
+    "past",
+    "perfect",
+    "pl",
+    "predadj_of",
+    "present",
+    "progressive",
+    "semantic_modifier_of",
+    "sg",
+    "superlative",
+    "time"
+])
 
 # ALL_TYPES_EXAMPLES = {"aux:plain": "will", "intj": "no", "verb:inf": "reach",
 #                       "prep_x_det": "with_x_the", "noun:acronym_x_punct": "a._x_.",
@@ -89,38 +136,38 @@ TAGS_INFO = {
 #                       "adv_x_aux:plain": "there_x_be",
 #                       "aux:plain_x_aux:plain": "have_x_be", "noun:acronym_x_adv": "1-1did_x_not"}
 
-TYPES = ["adj", "adv", "adv_x_aux:plain",
-         "adv_x_det", "aux:inf", "aux:pastpart",
-         "aux:plain", "aux:plain_x_adv", "aux:plain_x_aux:inf",
-         "aux:plain_x_aux:pastpart", "aux:plain_x_aux:plain",
-         "aux:prespart", "conj", "det", "intj", "noun:acronym",
-         "noun:acronym_x_adv", "noun:acronym_x_punct",
-         "noun:common", "noun:propernoun", "numeric",
-         "numeric_x_punct", "prep", "prep_x_det", "pron",
-         "pron_x_aux:plain", "prtcl", "punct", "symb",
-         "verb:inf", "verb:pastpart", "verb:plain", "verb:plain_x_adv",
-         "verb:plain_x_pron", "verb:prespart"]
-
+# TAGS = {
+#     "abstract": {
+#         "type": "boolean",
+#         "modifies": ["noun"]
+#     }
+# }
 
 def parse_systran(file):
     data = []
     with open(file, "r") as f:
-        for line in f:
+        for paragraph in f:
             words = []
-            line_text = []
-            for word in line.split(" "):
+            paragraph_text = []
+            for word in paragraph.split(" "):
                 word_details = word.split("-|-")
 
                 tags = {"relations": []}
                 for t in word_details[3].split(";"):
                     r = re.match(r"(.+)=(.+)", t)
                     if r is not None:
+                        tag = r.group(1)
                         v = r.group(2)
-                        if v[0] == '@':
-                            v = int(v[1:])
-                        # tags[r.group(1)] = v
-                        if r.group(1) in TAGS_INFO and TAGS_INFO[r.group(1)]["keep"]:
-                            tags[TAGS_INFO[r.group(1)]["cor"]] = v
+
+                        if tag in KEPT_TAGS:
+                            tag = TAGS_INFO[tag]
+                            if tag['type'] == "relation":
+                                v = int(v[1:])
+                            elif tag['type'] == "boolean":
+                                v = bool(int(v))
+
+                            tags[tag['id']] = v
+
                     else:
                         x = map(int, t.split("_@_"))
                         tags["relations"].append(tuple(x))
@@ -132,10 +179,10 @@ def parse_systran(file):
                     "type": word_details[2],
                     "tags": tags
                 })
-                line_text.append(word_details[0])
+                paragraph_text.append(word_details[0])
             data.append({
                 "id": len(data),
-                "text": " ".join(line_text),
+                "text": " ".join(paragraph_text),
                 "words": words
             })
     return data
