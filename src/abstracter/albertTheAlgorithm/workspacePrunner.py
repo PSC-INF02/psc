44e754e1259fbbf336a6937481f858bf5ad86738
Théophane Hufschmitt <theophane.hufschmitt@polytechnique.edu>
2015-05-15 09:09:12 +0200

    THE argorithm
    It's fucking buggy and hacky and produces ugly results, but it's there

---
+++ src/abstracter/albertTheAlgorithm/workspacePrunner.py
@@ -0,0 +1,39 @@
+"""
+@file workspacePrunner.py
+
+@brief THE algorithm
+"""
+
+from math import exp
+
+
+class workspacePrunner:
+
+    def __init__(self, workspace, concept_network):
+        self.wks = workspace
+        self.cnetwork = concept_network
+        for n, d in self.cnetwork.nodes_iter():
+            d['ic'] = int(exp(d['ic']) / exp(60))
+
+    def push_activation(self):
+        for n, d in self.wks.items():
+            if (d.__class__.__name__ != 'WordGroup' and
+                    d['norm'] in self.cnetwork):
+                self.cnetwork.activate([d['norm']], act=60)
+                for k in self.wks.network.out_arcs(n):
+                    self.cnetwork[d['norm']]['ic'] = (
+                        int(self.cnetwork[d['norm']]['ic'] * 1.1)
+                    )
+
+    def propagate(self, propagation_range=1):
+        for i in range(propagation_range):
+            self.cnetwork.propagate()
+
+    def prune(self, prune_level=1):
+        to_keep = [
+            i for i in self.cnetwork.get_activated_nodes(offset=prune_level+1)
+        ]
+        items = [i for i in self.wks.items()]
+        for n, d in items:
+            if d['norm'] not in to_keep:
+                self.wks.network.remove_node(n)
